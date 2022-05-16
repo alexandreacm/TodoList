8b0b2dc4d91540de7b040957ae8a21ed70ad7ab5
Alexandre Marques <alexandreacm.marques@gmail.com>
2022-05-16 09:21:59 -0300

    First commit.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,93 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useReducer } from "react";
+
+const initialState = {
+  todo: [],
+  loading: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "add-todo":
+      return {
+        todo: [
+          ...state.todo,
+          {
+            id: action.payload.id,
+            text: action.payload.text,
+          },
+        ],
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
+  const [list, setList] = useState([]);
+  const [text, setText] = useState("");
+  const [{ todo }, dispatch] = useReducer(reducer, initialState);
+
+  function onHandleAddTodo() {
+    let myTodo = {
+      id: Math.floor(Math.random() * 10),
+      text,
+    };
+
+    if (text === "") {
+      alert("Is required");
+      return;
+    }
+
+    dispatch({ type: "add-todo", payload: myTodo });
+    //setText([...list, myTodo]);
+    setText("");
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div>
+          <input
+            type="text"
+            placeholder="Typing something..."
+            style={{
+              width: 300,
+              height: 30,
+              borderRadius: 12,
+              marginBottom: 8,
+            }}
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <br />
+          <button
+            type="button"
+            style={{
+              width: 100,
+              height: 40,
+              background: "#6A5ACD",
+              color: "#FFF",
+              borderRadius: 10,
+            }}
+            onClick={onHandleAddTodo}
+          >
+            Add Todo
+          </button>
+
+          <p>
+            <table style={{ width: "100%" }}>
+              {todo.map((item) => {
+                return (
+                  <tr style={{ background: "#6495ED" }} key={item.id}>
+                    <td>{item.text}</td>
+                  </tr>
+                );
+              })}
+            </table>
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
